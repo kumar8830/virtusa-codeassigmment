1c9ca92f4f793116237e4f29f32ec3b083c3fa02
vvbkr <bharathvasipalli8830@yahoo.com>
2019-01-08 09:29:27 +0530

    Loading data locally

--- virtusa_app/ViewController.m
+++ virtusa_app/ViewController.m
@@ -16,40 +16,59 @@
 
 @implementation ViewController
 
+#pragma mark - ViewDidLoad
 - (void)viewDidLoad {
     [super viewDidLoad];
     
     UIView *footerCells  = [[UIView alloc]initWithFrame:CGRectMake(0, 0, 0, 0)];
     tabObj.tableFooterView = footerCells;
-    NSMutableURLRequest *urlRequest = [[NSMutableURLRequest alloc] initWithURL:[NSURL URLWithString:@"https://swapi.co/api/planets/"]];
+
+    NSUserDefaults *userDefaults = [NSUserDefaults standardUserDefaults];
+    tableArray = [userDefaults objectForKey:@"planetData"];
+    [userDefaults synchronize];
     
-    //create the Method "GET"
-    [urlRequest setHTTPMethod:@"GET"];
+    if (tableArray.count>=1) {
+        [self->tabObj reloadData];
+    }
     
-    NSURLSession *session = [NSURLSession sharedSession];
-    
-    NSURLSessionDataTask *dataTask = [session dataTaskWithRequest:urlRequest completionHandler:^(NSData *data, NSURLResponse *response, NSError *error)
+    else
     {
-        NSHTTPURLResponse *httpResponse = (NSHTTPURLResponse *)response;
-        if(httpResponse.statusCode == 200)
-        {
-            NSError *parseError = nil;
-            id json = [NSJSONSerialization JSONObjectWithData:data options:0 error:&parseError];
-            self->tableArray = [json objectForKey:@"results"];
-            if (self->tableArray.count>=1) {
-                dispatch_async(dispatch_get_main_queue(), ^{
-                   // self->tabObj.tableFooterView = UIView()
-                    [self->tabObj reloadData];
-                });
-            }
-            NSLog(@"The response is - %@",self->tableArray);
-        }
-        else
-        {
-            NSLog(@"Error");
-        }
-    }];
-    [dataTask resume];
+        //NSURLSession
+        NSMutableURLRequest *urlRequest = [[NSMutableURLRequest alloc] initWithURL:[NSURL URLWithString:@"https://swapi.co/api/planets/"]];
+        
+        //create the Method "GET"
+        [urlRequest setHTTPMethod:@"GET"];
+        
+        NSURLSession *session = [NSURLSession sharedSession];
+        
+        NSURLSessionDataTask *dataTask = [session dataTaskWithRequest:urlRequest completionHandler:^(NSData *data, NSURLResponse *response, NSError *error)
+                                          {
+                                              NSHTTPURLResponse *httpResponse = (NSHTTPURLResponse *)response;
+                                              if(httpResponse.statusCode == 200)
+                                              {
+                                                  NSError *parseError = nil;
+                                                  id json = [NSJSONSerialization JSONObjectWithData:data options:0 error:&parseError];
+                                                  self->tableArray = [json objectForKey:@"results"];
+                                                  if (self->tableArray.count>=1) {
+                                                      NSUserDefaults *userDefaults = [NSUserDefaults standardUserDefaults];
+                                                       [userDefaults setObject:self->tableArray forKey:@"planetData"];
+                                                      [userDefaults synchronize];
+
+                                                      dispatch_async(dispatch_get_main_queue(), ^{
+                                                          // self->tabObj.tableFooterView = UIView()
+                                                          [self->tabObj reloadData];
+                                                      });
+                                                  }
+                                                  NSLog(@"The response is - %@",self->tableArray);
+                                              }
+                                              else
+                                              {
+                                                  NSLog(@"Error");
+                                              }
+                                          }];
+        [dataTask resume];
+    }
+  
     // Do any additional setup after loading the view, typically from a nib.
 }
 
